cf5d810c2f33d91fcd94275c58b95b8cf7eec4b4
samAricha <samaricha001@gmail.com>
2024-08-28 18:16:14 +0300

    Initial commit

---
+++ src/test/kotlin/com/teka/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teka.bookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
